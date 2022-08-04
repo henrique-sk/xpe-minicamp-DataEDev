344b1f41757fc8a850adc0d839c8fd797d1f779b
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-08-03 23:04:58 -0300

    mega sena v1 parte 2

--- Modulo02/07-mega-sena-v1/js/script.js
+++ Modulo02/07-mega-sena-v1/js/script.js
@@ -6,21 +6,18 @@ function start() {
   addNumberToGame(3);
   addNumberToGame(4);
   addNumberToGame(5);
-  addNumberToGame(60);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(59);
-  addNumberToGame(62);
+  saveGame();
+  addNumberToGame(6);
+
+  saveGame();
+  saveGame();
 
   console.log(state.currentGame);
+  console.log(state.savedGames);
 }
 
 function addNumberToGame(numberToAdd) {
-  if (60 > numberToAdd < 1) {
+  if (numberToAdd < 1 || numberToAdd > 60) {
     console.error('Número inválido', numberToAdd);
     return;
   }
@@ -30,7 +27,54 @@ function addNumberToGame(numberToAdd) {
     return;
   }
 
+  if (isNumberInGame(numberToAdd)) {
+    console.error('Este número já está no jogo.', numberToAdd);
+    return;
+  }
+
   state.currentGame.push(numberToAdd);
+}
+
+function removeNumberFromGame(numberToRemove) {
+  if (numberToRemove < 1 || numberToRemove > 60) {
+    console.error('Número inválido', numberToRemove);
+    return;
+  }
+
+  var newGame = [];
+
+  for (var i = 0; i < state.currentGame.length; i++) {
+    var currentNumber = state.currentGame[i];
+
+    if (currentNumber === numberToRemove) {
+      continue;
+    }
+
+    newGame.push(currentNumber);
+  }
+
+  state.currentGame = newGame;
+}
+
+function isNumberInGame(numberToCheck) {
+  // if (state.currentGame.includes(numberToCheck)) {
+  //   return true;
+  // }
+  // return false;
+
+  return state.currentGame.includes(numberToCheck);
+}
+
+function saveGame() {
+  if (!isGameComplete()) {
+    console.error('O jogo não está completo');
+    return;
+  }
+  state.savedGames.push(state.currentGame);
+}
+
+function isGameComplete() {
+  return state.currentGame.length === 6;
 }
 
 start();
