f333e25909c24e7a3795dab1926b5bb6495ccdfb
Henrique Schilling Klein <58014127+henrique-sk@users.noreply.github.com>
2022-08-05 17:51:11 -0300

    teste prático

--- Modulo02/teste-pratico-imc-v2/js/script.js
+++ Modulo02/teste-pratico-imc-v2/js/script.js
@@ -17,6 +17,7 @@ function handdleButtonClick() {
   var inputWeight = document.querySelector('#input-weight');
   var inputHeight = document.querySelector('#input-height');
   var imcResult = document.querySelector('#imc-result');
+  var imcRange = document.querySelector('#imc-range');
 
   var weight = Number(inputWeight.value);
   var height = Number(inputHeight.value);
@@ -24,11 +25,20 @@ function handdleButtonClick() {
   var imc = calculateImc(weight, height);
   var formattedImc = imc.toFixed(2).replace('.', ',');
 
+  var range = rangeImc(imc);
+
   imcResult.textContent = formattedImc;
+  imcRange.textContent = range;
 }
 
 function calculateImc(weight, height) {
   return weight / (height * height);
+}
+
+function rangeImc(imc) {
+  if (20 < imc < 30) {
+    return 'Its OK!!';
+  }
 }
 
 start();
